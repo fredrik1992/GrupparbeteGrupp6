dba9a8dcbc136d38410e10c64c35451d9d49b798
HadiSy1991 <37807805+haik91@users.noreply.github.com>
2020-09-22 14:47:09 +0200

    Update Gepard.java

--- robotProjekt/src/com/company/Gepard.java
+++ robotProjekt/src/com/company/Gepard.java
@@ -1,4 +1,54 @@
 package com.company;
 
-public class Gepard {
+
+import java.util.Random;
+
+public class Gepard extends Djur {
+    private boolean resting;
+    private boolean eating;
+    private int direction;
+    private int x;
+    private int y;
+    int steps;
+    Random rand;
+
+    public Jepard(int x, int y) {
+        super(x, y);
+
+        resting = false;
+        eating = false;
+        rand = new Random();
+        steps = 1 + rand.nextInt(1);
+    }
+
+
+    public boolean isResting() {
+        return resting;
+    }
+
+
+    public boolean isEating() {
+        return eating;
+    }
+
+    public void Eat() {
+        eating = true;
+    }
+
+    public void rest() {
+        resting = true;
+    }
+
+    public void Stop_rest() {
+        resting = false;
+    }
+
+    @Override
+    public int getSteps() {
+        return steps;
+    }
+
 }
+
+}
+
