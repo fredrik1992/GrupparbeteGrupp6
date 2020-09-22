06efe1b2bffa03dcc38d1836b5855767eb7d8825
H0nken <71629427+H0nken@users.noreply.github.com>
2020-09-22 14:45:43 +0200

    Update Zebra.java

--- robotProjekt/src/com/company/Zebra.java
+++ robotProjekt/src/com/company/Zebra.java
@@ -1,4 +1,36 @@
 package com.company;
 
-public class Zebra {
+
+public class Zebra extends Djur {
+    private boolean dead;
+    private int steps;
+
+
+    public Zebra() {
+        super(x, y);
+        steps = 1;
+        dead = false;
+
+    }
+
+
+    public boolean getDead() {
+        return dead;
+    }
+
+    public void setDead() {
+        dead = true;
+    }
+
+    @Override
+    public int getSteps() {
+        return steps;
+    }
+
+    public void setSteps() {
+
+    }
+
+
 }
+
