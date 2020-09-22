1177fd0f85e186cc091892d7c4e99dbfb0087e81
aJohnson <52360575+aahJohnson@users.noreply.github.com>
2020-09-22 14:39:58 +0200

    Update Djur.java

--- robotProjekt/src/com/company/Djur.java
+++ robotProjekt/src/com/company/Djur.java
@@ -2,33 +2,35 @@ package com.company;
 
 import java.util.Random;
 
-public class Djur {
-        Random random = new Random();
-        int x;
-        int y;
-        int direction;
-        String dir;
+public abstract class Djur {
+    Random random = new Random();
+    int x;
+    int y;
+    int direction;
+    String dir;
 
-        public Djur() {
-            x = random.nextInt(10);
-            y = random.nextInt(10);
-            direction = random.nextInt(4);
+    public Djur() {
+        x = random.nextInt(10);
+        y = random.nextInt(10);
+        direction = random.nextInt(4);
+    }
+
+    public String Direction() {
+        switch (direction) {
+            case 0: // up
+                dir = "up";
+                break;
+            case 1: // down
+                dir = "down";
+                break;
+            case 2: // left
+                dir = "left";
+                break;
+            case 3: // right
+                dir = "right";
+                break;
         }
-        public String Direction() {
-            switch (direction) {
-                case 0: // up
-                    dir = "up";
-                    break;
-                case 1: // down
-                    dir = "down";
-                    break;
-                case 2: // left
-                    dir = "left";
-                    break;
-                case 3: // right
-                    dir = "right";
-                    break;
-            }
-            return dir;
-        }
+        return dir;
+    }
+    abstract int getSteps();
 }
