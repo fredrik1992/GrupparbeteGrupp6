00c823a0ed190882a9f745e6ac60d719ec1d15c8
fredrik1992 <68347541+fredrik1992@users.noreply.github.com>
2020-09-23 14:26:26 +0200

    Update Main.java

--- robotProjekt/src/com/company/Main.java
+++ robotProjekt/src/com/company/Main.java
@@ -1,21 +1,11 @@
-package com.company;
+package projektarbetet;
 
 public class Main {
 
     public static void main(String[] args) {
-        Gepard g = new Gepard();
-        Zebra z = new Zebra();
-        Djurlista d1 = new Djurlista();
-        d1.getInputOnAnimalQuantity();
-        Zebra[] zebra = new Zebra[d1.getQuantityZebras()];
-        Gepard[] gepard = new Gepard[d1.getQuantityCheetahs()];
-
-        d1.setVektors(zebra, gepard);
-        for (int i = 0 ; i< zebra.length;i++){
-            System.out.println(zebra[i].getSteps());
-        }
-
-
+       AI ai = new AI();
+       ai.createArrays();
+       ai.startGame();
 
     }
 }
