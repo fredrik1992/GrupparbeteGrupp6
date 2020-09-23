1316e241ded253758ef2dc378502c156e43f77e3
fredrik1992 <68347541+fredrik1992@users.noreply.github.com>
2020-09-23 14:24:59 +0200

    Update Djur.java

--- robotProjekt/src/com/company/Djur.java
+++ robotProjekt/src/com/company/Djur.java
@@ -1,14 +1,36 @@
+package projektarbetet;
+
+import java.util.Random;
+
 public abstract class Djur {
     Random random = new Random();
     int x;
     int y;
     int direction;
     String dir;
+    int turnCounter;
+    boolean rest;
 
     public Djur() {
         x = random.nextInt(10);
         y = random.nextInt(10);
         direction = random.nextInt(4);
+        rest = false;
+        turnCounter = 0;
+    }
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 
     public String Direction() {
