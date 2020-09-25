9563e4b67ba3d0874dd9bbce85fb974a26269b42
H0nken <71629427+H0nken@users.noreply.github.com>
2020-09-25 11:12:58 +0200

    Update AI.java

--- robotProjekt/src/com/company/AI.java
+++ robotProjekt/src/com/company/AI.java
@@ -1,4 +1,5 @@
-package projektarbetet;
+package com.company;
+
 
 public class AI {
     //variabler
@@ -19,6 +20,9 @@ public class AI {
         numberOfZebrasLeft = list.getQuantityZebras();
         zebra = new Zebra[list.getQuantityZebras()];
         gepard = new Gepard[list.getQuantityCheetahs()];
+
+        Djurlista namn = new Djurlista();
+        namn.setVektors(zebra, gepard);
     }
 
     public void startGame() {
@@ -27,56 +31,125 @@ public class AI {
     }
 
     public void gameLoop() {
-        for (int i = 0; i < zebra.length; i++) {
-            if (zebra[i].getDead() == false) {
+        while (numberOfZebrasLeft > 0) {
+            for (int i = 0; i < zebra.length; i++) {
+                if (zebra[i].getDead() == false) {
+                    System.out.println(numberOfZebrasLeft);
 
-                //send a object of zebra  to spelplan vektor (zebra)
+                    //send a object of zebra  to spelplan vektor (zebra)
 
+                }
             }
-        }
-        for (int i = 0; i < gepard.length; i++) {
-            if (gepard[i].isResting() == false) {
+            for (int i = 0; i < gepard.length; i++) {
+                if (gepard[i].isResting() == false) {
 
-                //send a object of zebra  to spelplan vektor (zebra)
+                    //send a object of zebra  to spelplan vektor (zebra)
 
+                }
             }
-        }
 
-        for (int j = 0; j < gepard.length; j++){
-            for (int i = 0; i < zebra.length; i++){
+            // This loop moves all the cheetahs
+            for (int i = 0; i < gepard.length; i++) {
+                if (gepard[i].getDirection().equals("up")) {
+                    if (gepard[i].getY() == 9) {
+                        while (gepard[i].getDirection().equals("up"))
+                            gepard[i].Direction();
+                    } else {
+                        gepard[i].setY(gepard[i].getSteps(), true);
+                    }
 
-                if (zebra[i].getX() == gepard[j].getX() && zebra[i].getY() == gepard[j].getY()){
-                    if (zebra[i].getDead() == false  && gepard[j].isResting() ==false){
-                        zebra[i].setDead();
-                        gepard[j].restAfterEating();
-                        numberOfZebrasLeft --;
+                } else if (gepard[i].getDirection().equals("down")) {
+                    if (gepard[i].getY() == 0) {
+                        while (gepard[i].getDirection().equals("down"))
+                            gepard[i].Direction();
+                    } else {
+                        gepard[i].setY(gepard[i].getSteps(), false);
+                    }
 
+                } else if (gepard[i].getDirection().equals("left")) {
+                    if (gepard[i].getX() == 0) {
+                        while (gepard[i].getDirection().equals("left"))
+                            gepard[i].Direction();
+                    } else {
+                        gepard[i].setX(gepard[i].getSteps(), false);
+                    }
+
+                } else if (gepard[i].getDirection().equals("right")) {
+                    if (gepard[i].getX() == 9) {
+                        while (gepard[i].getDirection().equals("right"))
+                            gepard[i].Direction();
+                    } else {
+                        gepard[i].setX(gepard[i].getSteps(), true);
                     }
                 }
+            }
 
+            // This loop moves all of the zebras
+            for (int i = 0; i < zebra.length; i++) {
+                if (zebra[i].getDirection().equals("up")) {
+                    if (zebra[i].getY() == 9) {
+                        while (zebra[i].getDirection().equals("up"))
+                            zebra[i].Direction();
+                    } else {
+                        zebra[i].setY(zebra[i].getSteps(), true);
+                    }
+
+                } else if (zebra[i].getDirection().equals("down")) {
+                    if (zebra[i].getY() == 0) {
+                        while (zebra[i].getDirection().equals("down"))
+                            zebra[i].Direction();
+                    } else {
+                        zebra[i].setY(zebra[i].getSteps(), false);
+                    }
+
+                } else if (zebra[i].getDirection().equals("left")) {
+                    if (zebra[i].getX() == 0) {
+                        while (zebra[i].getDirection().equals("left"))
+                            zebra[i].Direction();
+                    } else {
+                        zebra[i].setX(zebra[i].getSteps(), false);
+                    }
+
+                } else if (zebra[i].getDirection().equals("right")) {
+                    if (zebra[i].getX() == 9) {
+                        while (zebra[i].getDirection().equals("right"))
+                            zebra[i].Direction();
+                    } else {
+                        zebra[i].setX(zebra[i].getSteps(), true);
+                    }
+                }
+            }
+
+            for (int j = 0; j < gepard.length; j++) {
+                for (int i = 0; i < zebra.length; i++) {
+
+                    if (zebra[i].getX() == gepard[j].getX() && zebra[i].getY() == gepard[j].getY()) {
+                        if (zebra[i].getDead() == false && gepard[j].isResting() == false) {
+                            zebra[i].setDead();
+                            gepard[j].restAfterEating();
+                            numberOfZebrasLeft--;
+
+
+                        }
+                    }
+                }
             }
         }
+    }
+}
 
     /*for gepard[i]
         for  zebra[i]
             while (xebra gepad x,y && gepar.vila false)
             zebra (i).set(true)
             if gepard != vilar &&zebra !=död
-
-
-
-
-
      */
 
-        //äta
-        //gå
+//äta
+//gå
 
 
-        //ha
-
-    }
+//ha
 
 
-    //constructor
-}
+//constructor
