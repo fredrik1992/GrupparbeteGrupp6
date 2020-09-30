a330e8bc95b8af387b489025d9287bc6ceb905ea
fredrik1992 <68347541+fredrik1992@users.noreply.github.com>
2020-09-30 10:38:36 +0200

    Update Animals.java

--- robotProjekt/src/com/company/Animals.java
+++ robotProjekt/src/com/company/Animals.java
@@ -6,15 +6,13 @@ public abstract class Animals {
     Random random = new Random();
     int x;
     int y;
-
-
     int direction;
     String dir = "up";
     int turnCounter;
     boolean rest;
 
     public Animals() {
-        x = random.nextInt(10);
+        x = random.nextInt(10);//gives random x and y values to all animals
         y = random.nextInt(10);
         direction = random.nextInt(4);
         rest = false;
@@ -29,28 +27,28 @@ public abstract class Animals {
         return y;
     }
 
-    public void setX(int x, boolean xCheck) {
+    public void setX(int x, boolean xCheck) {//used when moving animal to check if x is moving left or right
         if (xCheck == true) {
             this.x += x;
 
         } else if (xCheck == false) {
             this.x -= x;
         }
-        this.x = x;
+
     }
 
-    public void setY(int y, boolean yCheck) {
+    public void setY(int y, boolean yCheck) {//used when moving animal to check if y is moving up or down.
 
-            if (yCheck == true) {
-                this.y += y;
+        if (yCheck == true) {
+            this.y += y;
 
-            } else if (yCheck == false) {
-                this.y -= y;
-            }
+        } else if (yCheck == false) {
+            this.y -= y;
+        }
     }
 
 
-    public String Direction() {
+    public String Direction() {//called to change direction to a random way to the value dir
         direction = random.nextInt(4);
         switch (direction) {
             case 0: // up
