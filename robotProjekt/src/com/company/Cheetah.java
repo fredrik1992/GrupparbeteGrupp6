495f2485039169f6d5ba55c3f381bfd7ecce728b
fredrik1992 <68347541+fredrik1992@users.noreply.github.com>
2020-09-30 10:24:48 +0200

    Update Cheetah.java

--- robotProjekt/src/com/company/Cheetah.java
+++ robotProjekt/src/com/company/Cheetah.java
@@ -19,7 +19,7 @@ public class Cheetah extends Animals {
     }
 
     @Override
-    public int getSteps() {
+    public int getSteps() {// called when moving an animal to keep track on its turn and if it has to rest and getting how many steps to take
         turnCounter++;
         setRest();
         Direction();
@@ -28,9 +28,9 @@ public class Cheetah extends Animals {
 
     public void restAfterEating() {
         this.rest = true;
-    }
+    }// used when the cheetah eats to make it rest
 
-    public void setRest() {
+    public void setRest() {//used to check if the cheetah has moved more then 3 turns then it have to rest
         if (turnCounter == 3) {
             rest = true;
             turnCounter = 0;
