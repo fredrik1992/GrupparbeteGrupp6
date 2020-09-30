71d5714348269321573524157325711ab7959d34
fredrik1992 <68347541+fredrik1992@users.noreply.github.com>
2020-09-30 10:35:49 +0200

    Update Main.java

--- robotProjekt/src/com/company/Main.java
+++ robotProjekt/src/com/company/Main.java
@@ -3,8 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-       AI ai = new AI();
-       ai.createArrays();
-       ai.startGame();
+        AI ai = new AI();// creates the ai  object
+        ai.createArrays();//calls the createVectors to set all the vectors
+        ai.startGame();//starts the game having all the vectors ready.
+
     }
 }
