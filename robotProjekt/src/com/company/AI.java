3db4ed88eea2115aeefb09d5497fc437ac06d8a5
fredrik1992 <68347541+fredrik1992@users.noreply.github.com>
2020-09-30 10:20:49 +0200

    Update AI.java

--- robotProjekt/src/com/company/AI.java
+++ robotProjekt/src/com/company/AI.java
@@ -1,55 +1,56 @@
-package com.company;
-
 public class AI {
-    //variables
+
     int numberOfZebrasLeft;
+    int round;
     Zebra[] zebra;
     Cheetah[] cheetah;
     AnimalList list;
-    int round;
+    GameBoard plan;
+    Message deathMessage;
 
-    GameBoard plan;  /// gameBoard object
-    
     public AI() {
         list = new AnimalList();
         numberOfZebrasLeft = 0;
     }
 
-    public void createArrays() {
-        list.getInputOnAnimalQuantity();
-        numberOfZebrasLeft = list.getQuantityZebras();
-        zebra = new Zebra[list.getQuantityZebras()];
-        cheetah = new Cheetah[list.getQuantityCheetahs()];
 
-        AnimalList name = new AnimalList();
-        name.setVectors(zebra, cheetah);
-    }
+    public void createVectors() {//this whole method is used to give our vectors a size and give them objects
+        list.getInputOnAnimalQuantity();//using the list object we call on the method getQuantity and sets values for zebras and cheetas
+        numberOfZebrasLeft = list.getQuantityZebras();//assigns the total number of zebras  alive at the start of the game
 
-    public void startGame() {
-        gameLoop();
-    }
+        zebra = new Zebra[list.getQuantityZebras()];//gives our zebra vector its size
+        cheetah = new Cheetah[list.getQuantityCheetahs()];//gives our cheetah vector its size
 
-    public void gameLoop() {
-        while (numberOfZebrasLeft > 0) {
-            round++;
-            System.out.println("\nRound: " + round);
-            for (Zebra value : zebra) {
-                if (!value.getDead()) {
-                    System.out.println(numberOfZebrasLeft);
+        //remove this  we allredy have an animalList vector AnimalList name = new AnimalList();
+        list.setArrays(zebra, cheetah);// using our list object we send our vectors into it's set vector method that
+        plan = new GameBoard(zebra, cheetah);
+    }                           //assigns objects to them
 
-                    //send a object of zebra to GamePlan vector (zebra)
+
+
+    public void startGame() {//this lope runs the entire game using values from the animal vectors
+        while (numberOfZebrasLeft > 0) {//checks if we reached the end of game''all zebras are dead''
+            if (numberOfZebrasLeft <= 2) {//slows the program down when 2 zebras are left in the game
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    Thread.currentThread().interrupt();
                 }
             }
-            //send a object of zebra to GamePlan vector (zebra)
+
+            round++;//adds to round to remember what round we are in
+            System.out.println("\nRound: " + round);//prints the current round
+            plan.print();
+
 
             // This loop moves all the cheetahs
-            for (Cheetah value : cheetah) {
-                switch (value.getDirection()) {
-                    case "up":
-                        if (value.getY() == 9) {
-                            while (value.getDirection().equals("up"))
-                                value.Direction();
-                        } else {
+            for (Cheetah value : cheetah) {//checks every cheetahs current direction and changes it if they are at the edge
+                switch (value.getDirection()) {//otherwise it just moves it
+                    case "up":// checks if the value up is in the current cheetah
+                        if (value.getY() == 9) {//checks if they have reached the edge
+                            while (value.getDirection().equals("up"))//changes the value to a random direction that
+                                value.Direction();                   //is not the current one
+                        } else {// if they are not at the edge the cheetah is moved
                             value.setY(value.getSteps(), true);
                         }
 
@@ -124,29 +125,30 @@ public class AI {
                 }
             }
 
-            for (Cheetah value : cheetah) {
-                for (Zebra item : zebra) {
-                    int zebrasToPrint = 0;
+            for (Cheetah value : cheetah) {// cheecks every cheetah against each zebra to see if they have the same
+                for (Zebra item : zebra) {//position (x,y)
+                    int zebrasToPrint = 0;//gives zebrasto print the initual value to check how many dies at one round
 
-                    if (item.getX() == value.getX() && item.getY() == value.getY()) {
-                        if (!item.getDead() && !value.isResting()) {
+                    if (item.getX() == value.getX() && item.getY() == value.getY()) {//checks x,y of cheetahs against zebras
+                        if (!item.getDead() && !value.isResting()) {//checks if a zebra is going do die
                             item.setDead();
                             value.restAfterEating();
-                            numberOfZebrasLeft--;
+                            numberOfZebrasLeft--;//this will eventually end the game
                             zebrasToPrint++;
                         }
                     }
-                    if (zebrasToPrint > 0) {
-                        printCheetah(zebrasToPrint);
+                    if (zebrasToPrint > 0) {//checks if we need to print enything this round
+                        printCheetah();
                     }
                 }
             }
         }
     }
 
-    public void printCheetah(int zebrasToPrint) {
-        Message m = new Message();
-        String message = m.Choice();
+
+    public void printCheetah() {
+        deathMessage = new Message();
+        String zebraWaysToDiePrint = deathMessage.Choice();
 
         System.out.print("\n" +
                 "\n" +
@@ -178,10 +180,6 @@ public class AI {
                 "                                                                        \\  \\__\n" +
                 "                                                                         \\ ____\\                                                                        \n" +
                 "                                                                             ");
-        System.out.printf("%s", message);
-        
-        //// GameBoard print
-         System.out.println("");
-        plan.print();
+        System.out.printf("%s", zebraWaysToDiePrint);
     }
 }
